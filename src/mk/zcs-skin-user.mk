9c599512ec2539e7f7dc7a0d5d4b4135a89ec82d
Enrico Weigelt, metux ITS <enrico.weigelt@vnc.biz>
2012-09-05 03:25:40 +0200

    skin building: additionally creating zip file

--- src/mk/zcs-skin-user.mk
+++ src/mk/zcs-skin-user.mk
@@ -8,11 +8,28 @@ all:	build
 
 build:	$(SKIN_ZIP)
 
-## zipfile for old-style deployment
-$(SKIN_ZIP):
-	@mkdir -p $(dir $(SKIN_ZIP))
-	@cd src/ && zip -r $(abspath $(SKIN_ZIP)) *
+_check:
+	@true
+ifeq ($(SKIN_NAME),)
+	@echo "SKIN_NAME undefined." >&2
+	@false
+endif
+ifeq ($(CURRENT_VERSION),)
+	@echo "CURRENT_VERSION undefined." >&2
+	@false
+endif
+
+tree:	_check
+	@rm -Rf tmp
+	@mkdir -p tmp/$(SKIN_NAME)
+	@cp --preserve -R src/* tmp/$(SKIN_NAME)/
+
+$(SKIN_ZIP):	tree
+	@mkdir -p $(dir $(SKIN_ZIP)) 
+	@cd tmp/ && zip -r $(abspath $(SKIN_ZIP)) *
 
 clean:
-	@rm -Rf $(SKIN_ZIP) $(SKIN_DIR)
+	@rm -Rf $(SKIN_ZIP) $(SKIN_DIR) tmp
 	@true
+
+.PHONY:	all build _check tree $(SKIN_ZIP) clean
