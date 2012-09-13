810b7229439576e2ec523015cd6cee66d27b3621
Enrico Weigelt, metux ITS <enrico.weigelt@vnc.biz>
2012-09-13 15:14:15 +0200

    new mk file: zcs-core-files.mk: installing arbitrary files (eg. configs)

---
+++ src/extensions-extra/zmpkg/mk/zcs-core-files.mk
@@ -0,0 +1,25 @@
+
+include $(TOPDIR)/common.mk
+
+JSP_CLASSPATH=`echo "$(JSP_BUILD_JARS)" | tr ' ' ':'`
+
+all:	build
+
+build:  jsp
+	@cp -R src/* $(IMAGE_ROOT)
+	@if [ -f $(IMAGE_ROOT)/mailboxd/webapps ]; then \
+		for i in `find $(IMAGE_ROOT)/mailboxd/webapps -name "*.js"` ; do \
+			cat "$$i" | gzip -9 > "$$i.gz" ; \
+		done	; \
+	fi
+
+clean:
+	@( cd src && find -not -type d ) | ( while read f ; do rm -f $(IMAGE_ROOT)/$$f ; done )
+	@( cd src && find -type d ) | sort -r | ( while read f ; do rmdir $(IMAGE_ROOT)/$$f 2>/dev/null || true ; done )
+
+jsp:
+ifneq ($(SKIP_JSP_COMPILE),y)
+	@for i in `find -name "*.jsp"` ; do JSP_CLASSPATH="$(JSP_CLASSPATH)" ZIMBRA_BUILD_ROOT="$(ZIMBRA_BUILD_ROOT)" $(COMPILE_JSP) $$i ; done
+endif
+
+.PHONY:	all build jsp clean
