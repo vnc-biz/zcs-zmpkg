e6d4d4dc85bc51c2430fe1503c0acd82ca4b4161
Enrico Weigelt, metux ITS <enrico.weigelt@vnc.biz>
2013-05-23 04:43:12 +0200

    main-upload-dpkg.mk: upload rule now calls $ZMPKG_UPLOAD_COMMAND

--- src/extensions-extra/zmpkg/mk/main-upload-dpkg.mk
+++ src/extensions-extra/zmpkg/mk/main-upload-dpkg.mk
@@ -1,32 +1,12 @@
 
-UPLOAD_DEPEND_RULES?=upload-dpkg upload-skins
+UPLOAD_DEPEND_RULES?=upload-dpkg
 
 upload:		$(UPLOAD_DEPEND_RULES)
 
 upload-check:
-	@if [ ! "$(REDMINE_UPLOAD_USER)" ];     then echo "REDMINE_UPLOAD_USER environment variable must be set"     ; exit 1 ; fi
-	@if [ ! "$(REDMINE_UPLOAD_PASSWORD)" ]; then echo "REDMINE_UPLOAD_PASSWORD environment variable must be set" ; exit 1 ; fi
-	@if [ ! "$(REDMINE_UPLOAD_URL)" ];      then echo "REDMINE_UPLOAD_URL variable must be set"                  ; exit 1 ; fi
-	@if [ ! "$(REDMINE_UPLOAD_PROJECT)" ];  then echo "REDMINE_UPLOAD_PROJECT variable must be set"              ; exit 1 ; fi
+	@if [ ! "$(ZMPKG_UPLOAD_COMMAND)" ]; then echo "ZMPKG_UPLOAD_COMMAND environment variable must be set" ; exit 1 ; fi
 
 upload-dpkg:	all upload-check
-	@zm_redmine_upload			\
-		-f "$(DEBIAN_PACKAGE)"		\
-		-l "$(REDMINE_UPLOAD_URL)"	\
-		-u "$(REDMINE_UPLOAD_USER)"	\
-		-w "$(REDMINE_UPLOAD_PASSWORD)"	\
-		-p "$(REDMINE_UPLOAD_PROJECT)"	\
-		-d "$(DEBIAN_PACKAGE)"
+	$(ZMPKG_UPLOAD_COMMAND) $(DEBIAN_PACKAGE)
 
-upload-skins:
-	@for i in `find image/zimlets-install -name "*.zip" -wholename "*/skins/*"` ; do \
-		zm_redmine_upload				\
-			-f "$$i"				\
-			-l "$(REDMINE_UPLOAD_URL)"		\
-			-u "$(REDMINE_UPLOAD_USER)"		\
-			-w "$(REDMINE_UPLOAD_PASSWORD)"		\
-			-p "$(REDMINE_UPLOAD_PROJECT)"		\
-			-d `basename "$$i"`		;	\
-		done
-
-.PHONY:	upload upload-dpkg upload-check upload-skins
+.PHONY:	upload upload-dpkg upload-check
